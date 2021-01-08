be645f217d1c746caa38f02e10c3f8cf80651859
nazir9681 <54324930+nazir9681@users.noreply.github.com>
2021-01-08 15:24:58 +0530

    Update settings.py

--- food_delivery_app/settings.py
+++ food_delivery_app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@fmo)&!x3ucl-83vitfu$s1-a)*$041tjao6sw%3gpz546-w!f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['zaika.herokuapp.com']
 
