5ffb764dcfa668035993a280dd5c6321339c7cc5
nazir <hussainnazir80@gmail.com>
2021-01-08 15:51:35 +0530

    changes

--- food_delivery_app/urls.py
+++ food_delivery_app/urls.py
@@ -13,8 +13,6 @@ urlpatterns = [
     path('notification/', include('notification.urls')),
     path('', include('ckeditor_uploader.urls')),
 
-    path('admin/', admin.site.urls),
-
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
